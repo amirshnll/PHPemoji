c91e6110d7ff244dec29dfee090f4d1d576e7cba
amirshnll <amirsh.nll@gmail.com>
2021-10-15 22:23:02 +0330

    Update test.php

--- test.php
+++ test.php
@@ -4,11 +4,11 @@ require("PHPemoji.php");
 
 $emoji = new PHPemoji();
 
-echo "Hello %v\n", $emoji->fire;
+echo "Hello ", $emoji->fire;
 
 $github = $emoji->github;
-echo "I love %v\n" . $github;
+echo "I love " . $github;
 
 echo $emoji->parse("Hello World :SmileOutlined:");
 $emoji->println("I Have Two :Eye: s");
-$emoji->print("PHP is :HeartOutlined:\n");
+$emoji->print("PHP is :HeartOutlined:");
